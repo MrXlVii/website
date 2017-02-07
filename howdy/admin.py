8bdf3558335cde4be556c6aa2415a986a27da7c5
MrXlVii <nale2892@gmail.com>
2017-02-06 22:27:45 -0500

    updates and created superuser

--- howdy/admin.py
+++ howdy/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Language, Project, ProjectInstance
 
 # Register your models here.
+
+admin.site.register(Project)
+admin.site.register(Language)
+admin.site.register(ProjectInstance)
