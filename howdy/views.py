ff197ce3ee00a1e4a8386d93c142e3639987218d
MrXlVii <nale2892@gmail.com>
2017-03-12 23:44:17 -0400

    created an edit feature

--- howdy/views.py
+++ howdy/views.py
@@ -52,9 +52,23 @@ def post_new(request):
             post.author = request.user
             post.posted_date = timezone.now()
             post.save()
-            return redirect('post_detail', pk=post.pk)
+            return redirect('howdy/post_detail.html', pk=post.pk)
     else:
         form = PostForm()
+    return render(request, 'howdy/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.posted_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
     return render(request, 'howdy/post_edit.html', {'form': form})
 
 def view_post(request, slug):   
